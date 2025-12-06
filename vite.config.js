2d571ae7e576e69ce6ddc7cf9aaef722203b26dd
varunwebcodes <varunnegiweb>
2025-12-06 14:48:05 +0530

    deploy settings

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
-import ghPages from 'vite-plugin-gh-pages'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss(), ghPages()],
+  plugins: [react(), tailwindcss()],
   base:"/CircularWebAnimation/"
 })
